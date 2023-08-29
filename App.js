0bcfa76fb962f4eb8f4167fd5e52c5ca0b9d1310
maritakia <maritadeery@gmail.com>
2023-08-29 12:55:07 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Inter-Black": require("./assets/Inter-Black.ttf"),
+    "Domine-Regular": require("./assets/Domine-Regular.ttf"),
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: "Inter-Black", fontSize: 30 }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text style={{ fontFamily: "Domine-Regular", fontSize: 30 }}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
